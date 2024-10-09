4e62f446b6d21fdde1dd8fab88a23b9107f14c72
HoTaN <id4acc2014@protonmail.com>
2024-10-09 19:16:15 +0330

    feat: :sparkles: I created Loader component with spinner and added styles
    I created a Loader component to display a loading spinner using the RotatingLines component from react-loader-spinner.
    The loader provides visual feedback while data is being fetched.
    I styled the loader using a CSS module (Loader.module.css) to ensure it is centered and occupies the appropriate height on the page, enhancing the user experience during loading states.

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,4 +1,5 @@
 import Card from "../components/Card";
+import Loader from "../components/Loader";
 import { useProducts } from "../context/ProductsProvider";
 import styles from "./ProductsPage.module.css";
 
@@ -8,7 +9,7 @@ function ProductsPage() {
   return (
     <div className={styles.container}>
       <div className={styles.products}>
-        {!products.length && <p>No products found</p>}
+        {!products.length && <Loader />}
         {products.map((product) => (
           <Card key={product.id} data={product} />
         ))}
