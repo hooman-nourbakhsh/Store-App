0b767dfa3bddd78c2b62c3c90aa60941d5f99caf
HoTaN <id4acc2014@protonmail.com>
2024-10-09 12:50:05 +0330

    feat: :sparkles: I added routing to App component using react-router-dom
    I implemented routing in the App component using react-router-dom.
    I created routes for ProductsPage, DetailsPage, CheckoutPage, and PageNotFound, and set up a redirect from the index route to the /products page.
    I also created individual files for each page component and imported them into the App component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import ProductsPage from "./pages/ProductsPage";
+import DetailsPage from "./pages/DetailsPage";
+import CheckoutPage from "./pages/CheckoutPage";
+import PageNotFound from "./pages/PageNotFound";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <Routes>
+      <Route index element={<Navigate to="/products" replace />} />
+      <Route path="/products" element={<ProductsPage />} />
+      <Route path="/products/:id" element={<DetailsPage />} />
+      <Route path="/checkout" element={<CheckoutPage />} />
+      <Route path="*" element={<PageNotFound />} />
+    </Routes>
+  );
 }
 
 export default App;
