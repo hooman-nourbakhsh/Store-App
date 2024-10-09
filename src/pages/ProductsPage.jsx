6b2f85494f997e1c849660321bd3ae98a86b43e6
HoTaN <id4acc2014@protonmail.com>
2024-10-09 19:29:34 +0330

    feat: :sparkles: I added a search feature to ProductsPage for product filtering
    I implemented a search feature in the ProductsPage component to allow users to filter products based on their input.
    I added an input field and a search button with an icon for user interaction.
    The search state is managed using the useState hook.
    The searchHandler function is set up to handle search actions, although it is currently empty.
    This feature enhances user experience by enabling product searches directly from the products list.

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,21 +1,36 @@
+import { useState } from "react";
+import { ImSearch } from "react-icons/im";
+
+import { useProducts } from "../context/ProductsProvider";
+
 import Card from "../components/Card";
 import Loader from "../components/Loader";
-import { useProducts } from "../context/ProductsProvider";
+
 import styles from "./ProductsPage.module.css";
 
 function ProductsPage() {
   const products = useProducts();
+  const [search, setSearch] = useState("");
+  const searchHandler = () => {};
 
   return (
-    <div className={styles.container}>
-      <div className={styles.products}>
-        {!products.length && <Loader />}
-        {products.map((product) => (
-          <Card key={product.id} data={product} />
-        ))}
+    <>
+      <div>
+        <input type="text" placeholder="Search..." value={search} onChange={(e) => setSearch(e.target.value.toLowerCase().trim())} />
+        <button onClick={searchHandler}>
+          <ImSearch />
+        </button>
       </div>
-      <div></div>
-    </div>
+      <div className={styles.container}>
+        <div className={styles.products}>
+          {!products.length && <Loader />}
+          {products.map((product) => (
+            <Card key={product.id} data={product} />
+          ))}
+        </div>
+        <div></div>
+      </div>
+    </>
   );
 }
 
