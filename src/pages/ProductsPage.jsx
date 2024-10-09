90aa37f41764c2e692f107eb2d9bd1d830b329a4
HoTaN <id4acc2014@protonmail.com>
2024-10-09 17:49:52 +0330

    feat: :sparkles: I used useProducts hook in ProductsPage and added CSS module for styling
    I integrated the custom useProducts hook into the ProductsPage component to fetch and display product data.
    The products are displayed dynamically, with a message shown when no products are available.
    I also created a CSS module (ProductsPage.module.css) to handle the styling, setting up a flex layout for the container and products, ensuring proper alignment and wrapping.

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,5 +1,20 @@
+import { useProducts } from "../context/ProductsProvider";
+import styles from "./ProductsPage.module.css";
+
 function ProductsPage() {
-  return <div>ProductsPage</div>;
+  const products = useProducts();
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.products}>
+        {!products.length && <p>No products found</p>}
+        {products.map((product) => (
+          <p key={product.id}>{product.title}</p>
+        ))}
+      </div>
+      <div></div>
+    </div>
+  );
 }
 
 export default ProductsPage;
