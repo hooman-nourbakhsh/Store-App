b29749003d218d4968837f5178c16763bec3d556
HoTaN <id4acc2014@protonmail.com>
2024-10-09 13:27:16 +0330

    feat: :sparkles: I created ProductContext to manage product data
    I created a ProductContext component to manage product data throughout the application.
    I set up a context using React's createContext and useEffect hooks, fetching product data from the API using Axios.
    The data is stored in a state variable and provided to the rest of the app through the context provider.
    The ProductContext is now available for consumption by child components.

---
+++ src/context/ProductsProvider.jsx
@@ -0,0 +1,23 @@
+import { createContext, useEffect, useState } from "react";
+import api from "../services/config";
+
+const ProductContext = createContext();
+
+function ProductsProvider({ children }) {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        setProducts(await api.get("/products"));
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    fetchProducts();
+  }, []);
+
+  return <ProductContext.Provider value={products}>{children}</ProductContext.Provider>;
+}
+
+export default ProductsProvider;
