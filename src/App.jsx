3ed0a90e9556111808c7ec9fb1b190d1f36a200b
HoTaN <id4acc2014@protonmail.com>
2024-10-11 11:16:03 +0330

    feat: :sparkles: Create CartProvider and wrap App with it
    I created a new CartProvider component using useReducer for managing cart state.
    This CartProvider now wraps the entire App component in App.jsx, allowing the cart state to be globally accessible throughout the application.

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,21 @@ import DetailsPage from "./pages/DetailsPage";
 import CheckoutPage from "./pages/CheckoutPage";
 import PageNotFound from "./pages/PageNotFound";
 import ProductsProvider from "./context/ProductsProvider";
+import CartProvider from "./context/CartProvider";
 
 function App() {
   return (
-    <ProductsProvider>
-      <Routes>
-        <Route index element={<Navigate to="/products" replace />} />
-        <Route path="/products" element={<ProductsPage />} />
-        <Route path="/products/:id" element={<DetailsPage />} />
-        <Route path="/checkout" element={<CheckoutPage />} />
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
-    </ProductsProvider>
+    <CartProvider>
+      <ProductsProvider>
+        <Routes>
+          <Route index element={<Navigate to="/products" replace />} />
+          <Route path="/products" element={<ProductsPage />} />
+          <Route path="/products/:id" element={<DetailsPage />} />
+          <Route path="/checkout" element={<CheckoutPage />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </ProductsProvider>
+    </CartProvider>
   );
 }
 
