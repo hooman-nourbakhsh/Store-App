92c33d1b7fdd66e1e2ce19812ab568499a8cb391
HoTaN <id4acc2014@protonmail.com>
2024-10-12 12:47:23 +0330

    feat: :sparkles: I developed the BasketSidebar component and integrated it into CheckoutPage
    I developed the BasketSidebar component, which displays the total price, item quantity, and checkout status.
    I integrated this component into the CheckoutPage to show the cart details. Additionally, I applied modular CSS styling to both the BasketSidebar and CheckoutPage components.

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -1,14 +1,24 @@
 import BasketCard from "../components/BasketCard";
+import BasketSidebar from "../components/BasketSidebar";
 import { useCart } from "../context/CartProvider";
+
+import styles from "./CheckoutPage.module.css";
 
 function CheckoutPage() {
   const [state, dispatch] = useCart();
 
   const clickHandler = (type, payload) => dispatch({ type, payload });
-
+  if (!state.itemsCounter) {
+    return (
+      <div className={styles.container}>
+        <p>Empty</p>
+      </div>
+    );
+  }
   return (
-    <div>
-      <div>
+    <div className={styles.container}>
+      <BasketSidebar state={state} clickHandler={clickHandler} />
+      <div className={styles.products} >
         {state.selectedItems.map((item) => (
           <BasketCard key={item.id} data={item} clickHandler={clickHandler} />
         ))}
