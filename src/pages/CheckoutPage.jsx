ea7a7b9e6a5f20fb6f7c5aacbbb46c1e3b5fbb25
HoTaN <id4acc2014@protonmail.com>
2024-10-12 11:35:08 +0330

    feat: :sparkles: I added CheckoutPage and BasketCard components with cart functionality
    I created the CheckoutPage component to display selected items from the cart using the newly added BasketCard component.
    The BasketCard allows users to increase, decrease, or remove items, with dispatch actions to handle these updates.
    Both components were styled using CSS modules for a clean interface.

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -1,5 +1,20 @@
+import BasketCard from "../components/BasketCard";
+import { useCart } from "../context/CartProvider";
+
 function CheckoutPage() {
-  return <div>CheckoutPage</div>;
+  const [state, dispatch] = useCart();
+
+  const clickHandler = (type, payload) => dispatch({ type, payload });
+
+  return (
+    <div>
+      <div>
+        {state.selectedItems.map((item) => (
+          <BasketCard key={item.id} data={item} clickHandler={clickHandler} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default CheckoutPage;
